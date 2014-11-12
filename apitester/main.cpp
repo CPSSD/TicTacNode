2b9a5213719f07ab2444ebb9acf41704cc61ab35
Conor <conor.griffin37@mail.dcu.ie>
2014-11-12 12:34:30 +0000

    Api Tester Functionality expanded

--- apitester/main.cpp
+++ apitester/main.cpp
@@ -85,6 +85,14 @@ string getData(string request)
     return replyJSON;
 }
 
+void finishgame(string p1, string p2)
+{
+    getData("move?id=" + gameid2 + "&position=5");
+    getData("move?id=" + gameid + "&position=1");
+    getData("move?id=" + gameid2 + "&position=0");
+    getData("move?id=" + gameid + "&position=7");
+}
+
 void testServer()
 {
     string serverResponse = getData("");
@@ -107,6 +115,72 @@ void testServer()
         cout << "Unknown Parameter : Passed" << endl;
     } else {
         cout << "Unknown Parameter : Failed" << endl;
+    }
+    serverResponse = getData("newGame?name=test1");
+    response = processJson(serverResponse);
+    string gameid = response.id;
+    string gameid2;
+    string turn = response.letter;
+    if (response.status == "okay" and (response.letter == 1 or response.letter == 2)) {
+        cout << "newGame request : Passed" << endl;
+    } else {
+        cout << "newGame request : Failed" << endl;
+    }
+    serverResponse = getData("next?id=wrong");
+    response = processJson(serverResponse);
+    if (response.status == "error" and response.code == 100 and response.message == "Other error") {
+        cout << "next request with invalid game id : Passed" << endl;
+    } else {
+        cout << "next request with invalid game id : Failed" << endl;
+    }
+    serverResponse = getData("move?id=wrong&position=5");
+    response = processJson(serverResponse);
+    if (response.status == "error" and response.code == 100 and response.message == "Other error") {
+        cout << "move request with invalid game id : Passed" << endl;
+    } else {
+        cout << "move request with invalid game id : Failed" << endl;
+    }
+    //Create a new player to play if our current gameid isn't a player 1
+    if (turn != 1) {
+        serverResponse = getData("newGame?name=test2");
+        response = processJson(serverResponse);
+        gameid = response.id;
+        serverResponse = getData("newGame?name=test3");
+        response = processJson(serverResponse);
+        gameid2 = response.id;
+    } else {
+        serverResponse = getData("newGame?name=test2");
+        response = processJson(serverResponse);
+        gameid2 = response.id;
+    }
+    serverResponse = getData("move?id=" + gameid + "&position=-1");
+    response = processJson(serverResponse);
+    if (response.status == "error" and response.code == 106 and response.message == "Invalid move") {
+        cout << "move request with out of bounds move : Passed" << endl;
+    } else {
+        cout << "move request with out of bounds move : Failed" << endl;
+    }
+    serverResponse = getData("move?id=" + gameid2 + "&position=4");
+    response = processJson(serverResponse);
+    if (response.status == "error" and response.code == 105 and response.message == "Not players turn") {
+        cout << "move request with not players turn : Passed" << endl;
+    } else {
+        cout << "move request with not players turn : Failed" << endl;
+    }
+    serverResponse = getData("move?id=" + gameid + "&position=4");
+    response = processJson(serverResponse);
+    if (response.status == "okay") {
+        cout << "move request with valid move : Passed" << endl;
+    } else {
+        cout << "move request with valid move : Failed" << endl;
+    }
+    finishgame(gameid, gameid2);
+    serverResponse = getData("next?id=" + gameid);
+    response = processJson(serverResponse);
+    if (response.status == "okay" and response.winner == 1) {
+        cout << "Next request winning game : Passed" << endl;
+    } else {
+        cout << "Next request winning game : Failed" << endl;
     }
 }
 
