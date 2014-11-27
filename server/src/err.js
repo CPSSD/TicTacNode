fdf434c9b5ca4faf292b7610be50376a93c86048
Wojtek Bednarzak <wojtek.bednarzal@gmail.com>
2014-11-27 01:23:38 +0000

    Added an option to specify custom message, if its not given it will be the default

--- server/src/err.js
+++ server/src/err.js
@@ -1,45 +1,51 @@
 // Error function
-module.exports = function(c){
+module.exports = function(c, msg){
   var ret = {};
   ret.status = "error";
   ret.code = c;
-  switch(c){
-    case 100:
-      ret.message = "Other error";
-      break;
 
-    case 101:
-      ret.message = "Missing parameter in request";
-      break;
+  if(typeof msg === 'undefined'){
+    switch(c){
+      case 100:
+        ret.message = "Other error";
+        break;
 
-    case 102:
-      ret.message = "Unknown parameter in request";
-      break;
+      case 101:
+        ret.message = "Missing parameter in request";
+        break;
 
-    case 103:
-      ret.message = "Unknown request";
-      break;
+      case 102:
+        ret.message = "Unknown parameter in request";
+        break;
 
-    case 104:
-      ret.message = "Server not ready or busy";
-      break;
+      case 103:
+        ret.message = "Unknown request";
+        break;
 
-    case 105:
-      ret.message = "Not players turn";
-      break;
+      case 104:
+        ret.message = "Server not ready or busy";
+        break;
 
-    case 106:
-      ret.message = "Invalid move";
-      break;
+      case 105:
+        ret.message = "Not players turn";
+        break;
 
-    case 107:
-      ret.message = "No such game";
-      break;
+      case 106:
+        ret.message = "Invalid move";
+        break;
 
-    case 108:
-      ret.message = "Game already has two players";
-      break;
+      case 107:
+        ret.message = "No such game";
+        break;
+
+      case 108:
+        ret.message = "Game already has two players";
+        break;
+    }
+  } else {
+    ret.message = msg;
   }
+
 
   return ret;
 };
