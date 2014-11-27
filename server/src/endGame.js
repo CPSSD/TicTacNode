0337b431f042619ebb5c274bfbcc2f25e3470bb9
Wojtek Bednarzak <wojtek.bednarzal@gmail.com>
2014-11-27 22:27:58 +0000

    Added endGame to finish unwanted games

--- server/src/endGame.js
+++ server/src/endGame.js
@@ -1,9 +1,27 @@
 var db = require('./db.js'),
     ret = require('./ret.js'),
     extra = require('./extra.js'),
-    err = require('./err.js');
+    err = require('./err.js'),
+
+    checkParam = extra.checkParam,
+    updateLastMove = extra.updateLastMove;
 
 // endGame function
-module.exports = function(req, q, s_t){
-
+module.exports = function(res, q, s_t){
+  if(!checkParam("secret",q)){
+    ret(res, err(101));
+  } else {
+    db.games.findAndModify({
+      query: {"player.secret": q.secret},
+      update: { $set: { winner: 0, finished: true} },
+      new: true,
+    }, function(er, data){
+      if(data === null){
+        ret(res, err(107));
+      } else {
+        updateLastMove(data.id);
+        ret(res, {"status": "okay"});
+      }
+    });
+  }
 };
