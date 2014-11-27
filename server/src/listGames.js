2ae223dd9999858bb12b16a3893185032bdc377e
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2014-11-27 16:43:50 +0000

    Changed to using ID to get all data straight from the database

--- server/src/listGames.js
+++ server/src/listGames.js
@@ -5,25 +5,23 @@ var db = require('./db.js'),
 
 // listGames function
 module.exports = function(res, q, s_t){
-  db.games.find({}, function(err, dataa){
+  db.games.find({ finished: false, player: { $size: 1 } }, function(er, dataa){
     var games = [];
 
     for(var c in dataa){
       var data = dataa[c];
-      var game = {};
-      if(!data.finished && data.player.length == 1){
-        game.id = "game-"+c;
-        game.name = data.player[0].name;
-        game.letter = data.player[0].letter == 1 ? 2 : 1;
-        game.private = data.private;
-        game.description = data.description;
+      var game = {
+        id: data.id,
+        name: data.player[0].name,
+        description: data.description,
+        letter: data.player[0].letter == 1 ? 2 : 1,
+        private: data.private
+      };
 
-        games.push(game);
-        game = {};
-      }
-
+      games.push(game);
+      game = {};
     }
 
-    ret(res, games);
+    ret(res, { "status":"okay", "games": games });
   });
 };
