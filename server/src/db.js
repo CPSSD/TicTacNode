9da462273bbaa30f1b90de54470f54b93271ebcf
Wojtek Bednarzak <wojtek.bednarzal@gmail.com>
2014-11-29 23:30:00 +0000

    Added indexes on any values that are queried

--- server/src/db.js
+++ server/src/db.js
@@ -3,4 +3,11 @@ var mongojs = require('mongojs'),
     config = require('../config.json');
 
 // Create the database connection and export it
-module.exports = mongojs(config.db.name, config.db.collections);
+var db = mongojs(config.db.name, config.db.collections);
+
+db.games.ensureIndex("players");
+db.games.ensureIndex("finished");
+db.games.ensureIndex("id");
+db.games.ensureIndex("last_move");
+
+module.exports = db;
